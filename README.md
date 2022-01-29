fa0ad64260940ddb9868fec6e6530e3e7d98a245
Apoorv Mishra <66416000+ApoorvGit@users.noreply.github.com>
2022-01-29 23:19:59 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Programming Memes Chrome extension
+This extension fetches the latest posts from my meme page on instagram and displays it in the extension popup. While coding whenever you feel bordem just click on the extension and enjoy memes made for programmers by programmers. 
---
+++ script.js
@@ -0,0 +1,12 @@
+(function(d, s, id){
+    var js; 
+    if (d.getElementById(id)) {
+        return;
+    } 
+    js = d.createElement(s); 
+    js.id = id; 
+    js.src = "https://embedsocial.com/cdn/ht.js"; 
+    d.getElementsByTagName("head")[0].appendChild(js);
+}
+(document, "script", "EmbedSocialHashtagScript")
+);
